bdbbd5b0d955c5a6a56bc96b79d62f0cfa2824bd
00012162 <wiut00012162@gmail.com>
2021-11-04 10:12:34 +0500

    123

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-word = {'one': 'bir',
-        'two': 'ikki',
-        'three': 'uch',
-        'four': 'to`rt'
-}
+# word = {'one': 'bir',
+#         'two': 'ikki',
+#         'three': 'uch',
+#         'four': 'to`rt'
+# }
+#
+# def translator(word):
+#     print(word['two'])
+#
+# translator(word)
 
-def translator(word):
-    print(word['two'])
 
-translator(word)
+#
+# result = (myFinalMarks['CSF'] + myFinalMarks['FunPro'] + myFinalMarks['WT']) / myFinalMarks.__len__()
+# print(result)
+#
+#
+myFinalMarks = {'CSF': 75,
+                'FunPro': 80,
+                'WT': 85}
+
+
+def calculate(key):
+    sum1 = 0
+
+    for i in key:
+        sum1 = sum1 + key[i]
+
+    _final_result = sum1 / myFinalMarks.__len__()
+    print(_final_result)
+
+
+calculate(myFinalMarks)
+
+
+# letters = {
+#             'a' =
+# }
